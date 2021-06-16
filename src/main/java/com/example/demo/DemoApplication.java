d567a213f3f614228367233621071391be280615
EugeneMaz10 <eugenengaira863@gmail.com>
2021-06-16 18:58:59 +0300

    Restful API

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class DemoApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+public  String hello()
+{
+	return "Hello World";
+
+}
 }
